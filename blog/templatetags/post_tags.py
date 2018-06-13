63ec33b5bed4e31e2f89cc84bf231f854c6352c2
XiaoFei <jack_970124@163.com>
2018-06-13 23:07:40 +0800

    新增列表点赞功能

--- blog/templatetags/post_tags.py
+++ blog/templatetags/post_tags.py
@@ -8,14 +8,14 @@ register = template.Library()
 def get_category_post(obj):
     """获取该目录下的所有博客"""
     post_list = Post.objects.filter(category=obj)
-    return post_list[0:10]
+    return post_list[0:14]
 
 
 @register.simple_tag
 def get_date_post(year, month):
     """获取该月份下的博客"""
     post_list = Post.objects.all().filter(created_time__year=year, created_time__month=month)
-    return post_list[:10]
+    return post_list[:14]
 
 @register.simple_tag
 def get_date_to_month(obj):
